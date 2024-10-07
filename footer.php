9a107a80d64389803b514fb93b7e9385a8e24b5d
Yoga Kusuma <yogakusum@gmail.com>
2024-10-07 07:05:52 +0700

    first init github

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!-- Footer -->
+<footer>
+    <div class="container-fluid d-flex align-items-center">
+        <div class="container">
+            <div class="row py-5">
+                <div class="col-md-7 text-lg-start text-center">
+                    <img src="Assets/Logo Icon.png" alt="Logo VR ITPLN">
+                    <a href="#" class="text-decoration-none ms-3">VR ITPLN</a>
+                </div>
+                <div class="col-md-5 text-center text-lg-end ms-auto mt-lg-0 mt-2">
+                    <a href="index.php" class="me-2">Beranda</a>
+                    <a href="tentang_kami.php" class="me-2">Tentang Kami</a>
+                    <a href="fasilitas.php" class="me-2">Fasilitas</a>
+                    <a href="pendidikan.php" class="me-2">Pendidikan</a>
+                    <a href="struktur_pengurus.php" class="me-2">Struktur Pengurus</a>
+                    <a href="kontak_kami.php" class="me-2">Kontak Kami</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 text-center">
+                    <p>
+                        Copyright by AAAAAAAA All Right Reserved 2025
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- My Script -->
+<script src="js/script.js"></script>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Bootstrap offline -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <!-- My Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Responsive -->
+    <link rel="stylesheet" href="css/Responsive.css">
+
+    <!-- Fonts Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>VR ITPLN</title>
+
+    <!-- Icon -->
+    <link rel="icon" href="Assets/Logo Icon.png" type="image/x-icon">
+</head>
+
+<body>
+
+    <!-- Hero Section -->
+    <section id="hero">
+
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light position-fixed w-100">
+            <div class="container">
+                <a class="navbar-brand" href="#hero">
+                    <img src="Assets/Logo Icon.png" alt="" width="45" class="d-inline-block align-text-top me-2">
+                    VR ITPLN
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item mx-2">
+                            <a class="nav-link active" aria-current="page" href="index.html">BERANDA</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="Tentang kami.html">TENTANG KAMI</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="fasilitas.html">FASILITAS</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="pendidikan.html">PENDIDIKAN</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="struktur pengurus.html">STRUKTUR PENGURUS</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="Kontak kami.html">KONTAK KAMI</a>
+                        </li>
+                    </ul>
+                    <div>
+                        <button class="button-primary" onclick="location.href='masuk.php'">Daftar</button>
+                        <button class="button-secondary" onclick="location.href='login.php'">Masuk</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
